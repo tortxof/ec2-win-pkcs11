2e7a5edb3c1a3521d4360eb53462ecebc0e82863
Daniel Jones <tortxof@gmail.com>
2021-07-29 11:54:51 -0400

    Add support for multiple tokens inserted

--- app.py
+++ app.py
@@ -10,15 +10,29 @@ import pkcs11
 @click.command()
 @click.option("-i", "--instance_id", prompt=True, required=True)
 @click.option("-p", "--profile", default="default", show_default=True)
+@click.option("-t", "--token", "token_serial", default=None, help="Token serial number")
 @click.option(
     "-l",
     "--lib",
     default="/usr/lib/x86_64-linux-gnu/opensc-pkcs11.so",
     show_default=True,
 )
-def main(instance_id, profile, lib):
+def main(instance_id, profile, token_serial, lib):
     lib = pkcs11.lib(lib)
-    token = lib.get_token()
+
+    if token_serial is not None:
+        token_serial = token_serial.encode()
+
+    try:
+        token = lib.get_token(token_serial=token_serial)
+    except pkcs11.MultipleTokensReturned:
+        click.echo("Multiple tokens found. Please provide token serial number.")
+        click.echo("Available token serial numbers:")
+        for token in lib.get_tokens():
+            click.echo(token.serial)
+        sys.exit()
+
+    click.echo(f"Using token serial number: {token.serial.decode()}")
 
     try:
         boto_session = boto3.Session(profile_name=profile)
